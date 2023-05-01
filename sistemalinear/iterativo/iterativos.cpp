01020e8272f0cff19e7d1b4b717a213d2042679d
Henrique de Lemos Braga <henriquelemosbraga@gmail.com>
2023-05-01 08:41:29 -0300

    Métodos jacobi e seidel funcionando

--- sistemalinear/iterativo/iterativos.cpp
+++ sistemalinear/iterativo/iterativos.cpp
@@ -1,23 +1,22 @@
 #include<iostream>
 #include<stdlib.h>
-#define MAX 10
 using namespace std;
 
-double **lermatriz(char *v,int *nl, int *nc){
-    FILE *arq;
+double **lermatriz(char *v, int *nl, int *nc){
+    FILE *arq = fopen(v, "r");
     double **m;
-    int a,b,i,j;
 
-    arq = fopen("matriz.dat","r");
-    fscanf(arq,"%d %d",&a,&b);
-    *nl = a;
-    *nc = b;
+    fscanf(arq, "%d %d", nl, nc);
 
-    *m = new double[*nl];
-    for(i=0;i<*nc;i++) m[i] = new double[*nc];
+    m = (double **)malloc(*nl*sizeof(double *));
+    for(int i=0;i<*nl;i++) m[i] = (double *)malloc(*nc*sizeof(double));
 
-    for(i=0;i<*nl;i++){
-        for(j=0;j<*nc;j++) fscanf(arq,"%lf",&m[i][j]);
+    for(int i=0; i<*nl;i++){
+        for(int j=0;j<*nc;j++){
+            fscanf(arq, "%lf", &m[i][j]);
+            //printf("%g\t",m[i][j]);
+        }
+        //puts("");
     }
 
     fclose(arq);
@@ -28,69 +27,69 @@ void imprimir(double **m, int nl, int nc){
     int i,j;
 
     for(i=0;i<nl;i++){
-        for(j=0;j<nc;j++) cout << m[i][j] << "\t";
+        for(j=0;j<nc;j++) printf("%g\t",m[i][j]);
         puts("");
     }
-
 }
 
 void jacobi(double **m,  double *v, int nl, int nc){
+    double *b;
+
+    b = (double *)malloc(nl*sizeof(double));
+
+    for(int i=0;i<nl;i++){
+        b[i] = m[i][nc-1]/m[i][i];
+        for(int j=0;j<nc-1;j++){
+            if(i!=j) b[i] -= (m[i][j]*v[j])/m[i][i]; 
+        }
+    }
+
+    for(int i=0;i<nl;i++) v[i] = b[i];
+
+    free(b);
+}
+
+
+void gauss_seidel(double **m,double *v, int nl, int nc){
     int i,j;
     double *b;
 
-    b = new double[nl];
+    b = (double *)malloc(nl*sizeof(double));
 
     for(i=0;i<nl;i++){
         b[i] = m[i][nc-1]/m[i][i];
         for(j=0;j<nl;j++){
             if(i!=j) b[i] -= (m[i][j]*v[j])/m[i][i]; 
         }
+        v[i] = b[i];
     }
 
-    for(i=0;i<nl;i++) v[i] = b[i];
-
-    delete(b);
+    free(b);
 }
 
-/*
-void gauss_seidel(double **m,double *v, int nl, int nc){
-    int i,j;
-    double *b;
-
-    b = new double[nl];
-
-    for(i=0;i<nl;i++) b[i] = m[i][nc-1];
-
-    for(i=0;i<nl;i++){
-        for(j=0;j<nl;j++){
-            if(i!=j) b[i] -= (m[i][j]*v[j]); 
-        }
-        v[i] = b[i]/m[i][i];
-    }
-
-    for(i=0;i<nl;i++) printf("%g\t",v[i]);
-    puts("");
-
-    delete(b);
-}
-*/
 
 main(int argc,char **argv){
-    double **m, j[3] = {2,3,5}, s[3] = {1,1,-1};
-    int nl, nc, i;
+    double **m, j[3] = {2,3,5}, s[3] = {2,3,5};
+    int nl, nc;
 
     m = lermatriz(argv[1],&nl,&nc);
+    imprimir(m,nl,nc);
 
-    cout << "---------------------------Jacobi-------------------------\n";
-    for(int i=0;i<MAX;i++){
-        cout << i << "\t";
-        for(int k=0;k<nl;k++) cout << j[k] << "\t";
+    
+    printf("---------------------------Jacobi-------------------------\n");
+    for(int i=0;i<10;i++){
+        printf("%d\t",i);
+        for(int k=0;k<nl;k++) printf("%g\t",j[k]);
         jacobi(m,j,nl,nc);
         puts("");
     }
     
-    
-    //printf("\n\n---------------------------Gauss-Seidel-------------------------\n");
-    //for(i=0;i<MAX;i++) gauss_seidel(m,s,nl,nc);
+    printf("---------------------------Gauss-Seidel-------------------------\n");
+    for(int i=0;i<10;i++){
+        printf("%d\t",i);
+        for(int k=0;k<nl;k++) printf("%g\t",s[k]);
+        gauss_seidel(m,s,nl,nc);
+        puts("");
+    }
 
 }
